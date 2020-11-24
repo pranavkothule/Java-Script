0eb8b15e45582f766e82a3249fd33d16d297e5a2
Pranav Kothule <pranavrk26@gmail.com>
2020-11-24 20:48:08 +0530

    Update typeconversion.js

--- typeconversion.js
+++ typeconversion.js
@@ -1,13 +1,14 @@
 //type conversion
 console.log("****Type converison****");
 
-
+/***********************************/
 /* let myVar=34;
 console.log(myVar);
 console.log(typeof(myVar));
  */
 
- //String conversion
+/***********************************/
+//String conversion
 let myVar1=String(34);
 console.log(myVar1,(typeof myVar1));
 
@@ -29,7 +30,7 @@ let oLitrals=String({
 })
 console.log(oLitrals,(typeof oLitrals));
 
-
+/***********************************/
 //number converion
 let li=Number("1233");
 li=Number("345d43");
@@ -37,18 +38,12 @@ li=Number(true);
 li=Number([1,2,3,4,5]);
 console.log(li,(typeof li));
 
-
 let numF=parseFloat(30.456);
 //let numI=parseInt(30.456);
 console.log(numF.toFixed(2),(typeof numF));
 
-
-
-
-
-
+/***********************************/
 //type coercion
-
 let mystr="87";
 let mynum=98;
 console.log(mystr+mynum);
